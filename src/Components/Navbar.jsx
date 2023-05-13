31fc7b7c00abbaf24a15dfd869b5c023511a4dda
jonheri <jonheri001@gmail.com>
2023-05-13 11:45:53 +0700

    First Commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="bg-red">
+        <h3 className="text-white">Navbar Home About</h3>
+      </div>
+    </>
+  );
+}
---
+++ src/Pages/Product/Data.js
@@ -0,0 +1,10 @@
+let dataArray = [
+  {
+    id: 123,
+    productName: "ladsjf",
+  },
+  {
+    id: 124,
+    productName: "ladsjf",
+  },
+];
